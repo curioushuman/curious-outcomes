31617efb6472474d28f08a729583db316e8bb319
Mike Kelly <mike@curioushuman.com.au>
2022-06-29 17:48:50 +1000

    refactor(events): :zap: Improving cold starts

--- apps/system/src/main.ts
+++ apps/system/src/main.ts
@@ -1,9 +1,16 @@
-// import { INestApplicationContext } from '@nestjs/common';
+import { INestApplicationContext } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
 import { EventsController } from './modules/events/infra/events.controller';
 import { EventsModule } from './modules/events/events.module';
+
+/**
+ * Hold a reference to your Nest app outside of the bootstrap function
+ * to minimize cold starts
+ * https://towardsaws.com/serverless-love-story-nestjs-lambda-part-i-minimizing-cold-starts-4ba513e5ce02
+ */
+let lambdaApp: INestApplicationContext;
 
 /**
  * Standalone Nest application for Serverless context
@@ -19,12 +26,19 @@ async function bootstrap() {
   return await NestFactory.createApplicationContext(EventsModule);
 }
 
+async function waitForApp() {
+  if (!lambdaApp) {
+    lambdaApp = await bootstrap();
+  }
+  return lambdaApp;
+}
+
 // export async function handler(event: APIGatewayEvent, context: Context) {
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  const app = await bootstrap();
+  const app = await waitForApp();
   const eventsController = app.get(EventsController);
 
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
