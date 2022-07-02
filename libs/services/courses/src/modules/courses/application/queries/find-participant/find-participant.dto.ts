fbec02fbd4b6440e4d1d966fc96db0db233bb8b3
Mike Kelly <mike@curioushuman.com.au>
2022-07-02 15:53:28 +1000

    refactor(courses): :fire: Stripping back, to build back

--- libs/services/courses/src/modules/courses/application/queries/find-participant/find-participant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Record, Static } from 'runtypes';
-
-import { ExternalId } from '../../../domain/value-objects/external-id';
-
-export const FindParticipantDto = Record({
-  externalId: ExternalId,
-});
-
-export type FindParticipantDto = Static<typeof FindParticipantDto>;
