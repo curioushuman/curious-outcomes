00b0dde4081b3efc26fff33af43e0b3a054023fe
Mike Kelly <mike@curioushuman.com.au>
2022-07-03 14:16:38 +1000

    refactor: :recycle: Rearranging shared libs for inclusion in lambda layers

--- libs/shared/co-common/jest.config.ts
+++ libs/shared/co-common/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'loggable',
-  preset: '../../jest.preset.js',
+  displayName: 'co-common',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/loggable',
+  coverageDirectory: '../../../coverage/libs/co-common',
 };
--- libs/shared/error-factory/jest.config.ts
+++ libs/shared/error-factory/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'fp-ts-utils',
-  preset: '../../jest.preset.js',
+  displayName: 'error-factory',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/fp-ts-utils',
+  coverageDirectory: '../../../coverage/libs/error-factory',
 };
