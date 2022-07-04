199ed9d4ecf7fc2a3133815a679524f2a542ec66
Mike Kelly <mike@curioushuman.com.au>
2022-07-05 06:39:13 +1000

    refactor(courses): :truck: courses to co-courses

--- libs/services/co-courses/jest.config.ts
+++ libs/services/co-courses/jest.config.ts
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/services/courses',
+  coverageDirectory: '../../../coverage/libs/services/co-courses',
 };
