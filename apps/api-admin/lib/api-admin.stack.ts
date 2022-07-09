efc2bab1964bec190e8577f9c42ec90d5939b07d
Mike Kelly <mike@curioushuman.com.au>
2022-07-09 18:36:49 +1000

    test(api-admin): :white_check_mark: Basic tests for /courses/hook

--- apps/api-admin/lib/api-admin.stack.ts
+++ apps/api-admin/lib/api-admin.stack.ts
@@ -82,7 +82,7 @@ export class ApiAdminStack extends cdk.Stack {
     const courses = api.root.addResource('courses');
 
     /**
-     * Resources for the /courses endpoint
+     * Resources/methods for the /courses endpoint
      */
     /**
      * GET /courses/{eventType}/{courseId}?status={status}
@@ -92,127 +92,124 @@ export class ApiAdminStack extends cdk.Stack {
     const coursesHookTypeAndId = coursesHookType.addResource('{courseId}');
     // integration with SNS topic for external events
     // source: https://github.com/cdk-patterns/serverless/blob/main/the-big-fan/typescript/lib/the-big-fan-stack.ts
-    coursesHookTypeAndId.addMethod('GET');
-    // coursesHookTypeAndId.addMethod('GET',
-    //   new apigateway.AwsIntegration({
-    //     service: 'sns',
-    //     integrationHttpMethod: 'POST',
-    //     path: `${cdk.Aws.ACCOUNT_ID}/${topicExternalEvents.topicName}`,
-    //     options: {
-    //       credentialsRole: apiSnsRole,
-    //       // Tell api gw to send our payload as query params
-    //       // TODO: seems standard, but why?
-    //       requestParameters: {
-    //         'integration.request.header.Content-Type':
-    //           "'application/x-www-form-urlencoded'",
-    //       },
-    //       // NOTE: the following source has an example of using VTL:foreach to include
-    //       //       the params of the original request, in the JSON of the message.
-    //       //       https://medium.com/@gmalaguti/aws-api-gateway-integration-treks-sns-d47543cb0413
-    //       //       VTL: https://velocity.apache.org/engine/devel/vtl-reference.html
-    //       requestTemplates: {
-    //         'application/json':
-    //           '#set($allParams = $input.params())\n' +
-    //           "#set($pathParams = $allParams.get('path'))\n" +
-    //           'Action=Publish&' +
-    //           "TargetArn=$util.urlEncode('" +
-    //           topicExternalEvents.topicArn +
-    //           "')&" +
-    //           'MessageStructure=json&' +
-    //           // NOTE: when using JSON as MessageStructure (for SNS) you must define a message
-    //           //       per (consuming) AWS service; AND a default message is required.
-    //           //       https://docs.aws.amazon.com/sns/latest/api/API_Publish.html
-    //           // TODO:
-    //           // - [ ] SQS, similar to Lambda
-    //           // - [ ] improve the default message
-    //           'Message={' +
-    //           '"lambda":"{\n' +
-    //           '#foreach($paramName in $pathParams.keySet())\n' +
-    //           '"$paramName" : "$util.escapeJavaScript($pathParams.get($paramName))"\n' +
-    //           '#if($foreach.hasNext),#end\n' +
-    //           '#end\n' +
-    //           '"default":"$util.urlEncode($input.params(\'eventType\')):$util.urlEncode($input.params(\'courseId\'))"' +
-    //           +'}&' +
-    //           'Version=2010-03-31&' +
-    //           'MessageAttributes.entry.1.Name=type&' +
-    //           'MessageAttributes.entry.1.Value.DataType=String&' +
-    //           "MessageAttributes.entry.1.Value.StringValue=$util.urlEncode($input.params('eventType'))&" +
-    //           'MessageAttributes.entry.2.Name=status&' +
-    //           'MessageAttributes.entry.2.Value.DataType=String&' +
-    //           "MessageAttributes.entry.2.Value.StringValue=$util.urlEncode($input.params('status'))",
-    //       },
-    //       passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-    //       integrationResponses: [
-    //         {
-    //           // if the return code from SNS is 200, use the success response
-    //           statusCode: '200',
-    //           responseTemplates: {
-    //             // Just respond with a generic message
-    //             'application/json': JSON.stringify({
-    //               message: 'message added to topic',
-    //             }),
-    //           },
-    //         },
-    //         {
-    //           // If the response includes the word Error, use the error response
-    //           // source: https://github.com/cdk-patterns/serverless/blob/main/the-big-fan/typescript/lib/the-big-fan-stack.ts
-    //           // prettier-ignore
-    //           // eslint-disable-next-line no-useless-escape
-    //           selectionPattern: "^\[Error\].*",
-    //           statusCode: '400',
-    //           responseTemplates: {
-    //             'application/json': JSON.stringify({
-    //               state: 'error',
-    //               message:
-    //                 "$util.escapeJavaScript($input.path('$.errorMessage'))",
-    //             }),
-    //           },
-    //           responseParameters: {
-    //             'method.response.header.Content-Type': "'application/json'",
-    //             'method.response.header.Access-Control-Allow-Origin': "'*'",
-    //             'method.response.header.Access-Control-Allow-Credentials':
-    //               "'true'",
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   }),
-    //   {
-    //     methodResponses: [
-    //       //We need to define what models are allowed on our method response
-    //       {
-    //         // Successful response from the integration
-    //         statusCode: '200',
-    //         // Define what parameters are allowed or not
-    //         responseParameters: {
-    //           'method.response.header.Content-Type': true,
-    //           'method.response.header.Access-Control-Allow-Origin': true,
-    //           'method.response.header.Access-Control-Allow-Credentials': true,
-    //         },
-    //         // Validate the schema on the response
-    //         responseModels: {
-    //           'application/json': responseModel,
-    //         },
-    //       },
-    //       {
-    //         // Same thing for the error responses
-    //         statusCode: '400',
-    //         responseParameters: {
-    //           'method.response.header.Content-Type': true,
-    //           'method.response.header.Access-Control-Allow-Origin': true,
-    //           'method.response.header.Access-Control-Allow-Credentials': true,
-    //         },
-    //         responseModels: {
-    //           'application/json': errorResponseModel,
-    //         },
-    //       },
-    //     ],
-    //   }
-    // );
+    coursesHookTypeAndId.addMethod(
+      'GET',
+      new apigateway.AwsIntegration({
+        service: 'sns',
+        integrationHttpMethod: 'POST',
+        path: `${cdk.Aws.ACCOUNT_ID}/${topicExternalEvents.topicName}`,
+        options: {
+          credentialsRole: apiSnsRole,
+          // Tell api gw to send our payload as query params
+          // TODO: seems standard, but why?
+          requestParameters: {
+            'integration.request.header.Content-Type':
+              "'application/x-www-form-urlencoded'",
+          },
+          requestTemplates: {
+            'application/json':
+              '#set($allParams = $input.params())\n' +
+              "#set($pathParams = $allParams.get('path'))\n" +
+              'Action=Publish&' +
+              "TargetArn=$util.urlEncode('" +
+              topicExternalEvents.topicArn +
+              "')&" +
+              'MessageStructure=json&' +
+              // NOTE: when using JSON as MessageStructure (for SNS) you must define a message
+              //       per (consuming) AWS service; AND a default message is required.
+              //       https://docs.aws.amazon.com/sns/latest/api/API_Publish.html
+              // TODO:
+              // - [ ] SQS, similar to Lambda
+              // - [ ] improve the default message
+              'Message={' +
+              '"lambda":"{\n' +
+              '#foreach($paramName in $pathParams.keySet())\n' +
+              '\\"$paramName\\" : \\"$util.escapeJavaScript($pathParams.get($paramName))\\"\n' +
+              '#if($foreach.hasNext),#end\n' +
+              '#end\n' +
+              '#if($input.params(\'status\'))\\"status\\":\\"$util.escapeJavaScript($input.params(\'status\'))\\"#end\n' +
+              '"default":"$util.urlEncode($input.params(\'eventType\')):$util.urlEncode($input.params(\'courseId\'))"' +
+              +'}&' +
+              'Version=2010-03-31&' +
+              'MessageAttributes.entry.1.Name=type&' +
+              'MessageAttributes.entry.1.Value.DataType=String&' +
+              "MessageAttributes.entry.1.Value.StringValue=$util.urlEncode($input.params('eventType'))&" +
+              'MessageAttributes.entry.2.Name=status&' +
+              'MessageAttributes.entry.2.Value.DataType=String&' +
+              "MessageAttributes.entry.2.Value.StringValue=$util.urlEncode($input.params('status'))",
+          },
+          passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+          integrationResponses: [
+            {
+              // if the return code from SNS is 200, use the success response
+              statusCode: '200',
+              responseTemplates: {
+                // Just respond with a generic message
+                'application/json': JSON.stringify({
+                  message: 'message added to topic',
+                }),
+              },
+            },
+            {
+              // If the response includes the word Error, use the error response
+              // source: https://github.com/cdk-patterns/serverless/blob/main/the-big-fan/typescript/lib/the-big-fan-stack.ts
+              // prettier-ignore
+              // eslint-disable-next-line no-useless-escape
+              selectionPattern: "^\[Error\].*",
+              statusCode: '400',
+              responseTemplates: {
+                'application/json': JSON.stringify({
+                  state: 'error',
+                  message:
+                    "$util.escapeJavaScript($input.path('$.errorMessage'))",
+                }),
+              },
+              responseParameters: {
+                'method.response.header.Content-Type': "'application/json'",
+                'method.response.header.Access-Control-Allow-Origin': "'*'",
+                'method.response.header.Access-Control-Allow-Credentials':
+                  "'true'",
+              },
+            },
+          ],
+        },
+      }),
+      {
+        methodResponses: [
+          //We need to define what models are allowed on our method response
+          {
+            // Successful response from the integration
+            statusCode: '200',
+            // Define what parameters are allowed or not
+            responseParameters: {
+              'method.response.header.Content-Type': true,
+              'method.response.header.Access-Control-Allow-Origin': true,
+              'method.response.header.Access-Control-Allow-Credentials': true,
+            },
+            // Validate the schema on the response
+            responseModels: {
+              'application/json': responseModel,
+            },
+          },
+          {
+            // Same thing for the error responses
+            statusCode: '400',
+            responseParameters: {
+              'method.response.header.Content-Type': true,
+              'method.response.header.Access-Control-Allow-Origin': true,
+              'method.response.header.Access-Control-Allow-Credentials': true,
+            },
+            responseModels: {
+              'application/json': errorResponseModel,
+            },
+          },
+        ],
+      }
+    );
 
     /**
      * Outputs
      */
-    // new cdk.CfnOutput(this, 'apiUrl', { value: api.urlForPath() });
+    new cdk.CfnOutput(this, 'apiUrl', { value: api.urlForPath() });
   }
 }
--- apps/api-admin/lib/test/api-admin.spec.ts
+++ apps/api-admin/lib/test/api-admin.spec.ts
@@ -1,4 +1,4 @@
-import { Template } from 'aws-cdk-lib/assertions';
+import { Capture, Match, Template } from 'aws-cdk-lib/assertions';
 import * as cdk from 'aws-cdk-lib';
 import { ApiAdminStack } from '../api-admin.stack';
 
@@ -21,8 +21,51 @@ describe('ApiAdminStack', () => {
     template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
   });
 
-  // TODO: get rid of this one, not a very good test
-  it('Should contain a single endpoint', () => {
-    template.resourceCountIs('AWS::ApiGateway::Resource', 4);
+  describe('Should support the following endpoints', () => {
+    describe('/courses/hook/{eventType}/{courseId}', () => {
+      const integrationCapture = new Capture();
+      const methodResponsesCapture = new Capture();
+      const resourceParentRegex = 'apiadmincourseshookeventType[A-Z0-9]+';
+      const resourceIdRegex = 'apiadmincourseshookeventTypecourseId[A-Z0-9]+';
+
+      it('Should exist', () => {
+        // TODO: I would prefer to be more specific with this test
+        // Difficult to do as a one off, but I might be able to write
+        // some supporting functions to test such things.
+        // e.g. https://thomasstep.com/blog/how-to-write-aws-cdk-tests
+        template.hasResourceProperties('AWS::ApiGateway::Resource', {
+          PathPart: '{courseId}',
+          ParentId: {
+            Ref: Match.stringLikeRegexp(resourceParentRegex),
+          },
+        });
+      });
+
+      describe('GET', () => {
+        it('Should exist', () => {
+          template.hasResourceProperties('AWS::ApiGateway::Method', {
+            HttpMethod: 'GET',
+            ResourceId: {
+              Ref: Match.stringLikeRegexp(resourceIdRegex),
+            },
+            Integration: integrationCapture,
+            MethodResponses: methodResponsesCapture,
+          });
+        });
+        test('With credentials', () => {
+          expect(integrationCapture.asObject().Credentials).toBeDefined();
+        });
+        // TODO: I'm not sure exactly how to do this
+        test.todo('With a request template that includes lambda');
+        test.todo('With a request template that includes sqs');
+        test.todo('With a request template that includes default');
+        test('With two method responses', () => {
+          const methodResponses = methodResponsesCapture.asArray();
+          expect(methodResponses.length).toBe(2);
+          expect(methodResponses[0].StatusCode).toBe('200');
+          expect(methodResponses[1].StatusCode).toBe('400');
+        });
+      });
+    });
   });
 });
