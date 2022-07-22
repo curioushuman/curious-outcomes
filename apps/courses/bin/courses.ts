1f58a43742eeeaf9341e2f94b7f909d61fc87f15
Mike Kelly <mike@curioushuman.com.au>
2022-07-22 20:04:02 +1000

    build: Adding some npm scripts to deploy everything locally

--- apps/courses/bin/courses.ts
+++ apps/courses/bin/courses.ts
@@ -1,11 +1,29 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+
 import { CoursesStack } from '../lib/courses.stack';
 
-const devEnv = {
-  account: '793735516609',
-  region: 'ap-southeast-2',
-};
+import { resolve as pathResolve } from 'path';
+import * as dotenv from 'dotenv';
 
+// these will pick up whatever profile you run your CDK commands with
+// see aw cli configuration
+let account = process.env.CDK_DEFAULT_ACCOUNT;
+let region = process.env.CDK_DEFAULT_REGION;
+
+// this will pick up our local environments
+if (process.env.NODE_ENV === 'local') {
+  // cwd will be the app root
+  dotenv.config({ path: pathResolve(process.cwd(), '../../.env') });
+  account = process.env.CDK_DEPLOY_ACCOUNT;
+  region = process.env.CDK_DEPLOY_REGION;
+}
+
+// creating an app in cloud or local
 const app = new cdk.App();
-new CoursesStack(app, 'CoursesStack', { env: devEnv });
+new CoursesStack(app, 'CoursesStack', {
+  env: {
+    account,
+    region,
+  },
+});
